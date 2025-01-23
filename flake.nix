f2fdf67196519ac1088a987182522b27c5c28411
Lucas Franceschino <lucas@cryspen.com>
2025-01-23 08:57:54 +0100

    bundles

--- flake.nix
+++ flake.nix
@@ -1,69 +1,63 @@
 {
-  inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs = { flake-utils.url = "github:numtide/flake-utils"; };
 
-  outputs = {
-    flake-utils,
-    nixpkgs,
-    ...
-  } @ inputs:
-    flake-utils.lib.eachDefaultSystem (
-      system: let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+  outputs = { flake-utils, nixpkgs, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
         z3-factory = import ./package.nix;
-        # z3-factory = pkgs.callPackage ./package.nix {};
         versions = builtins.fromJSON (builtins.readFile ./versions.json);
-        z3-set = pkgs.lib.listToAttrs (map (
-            o: let
-              version = pkgs.lib.replaceStrings ["."] ["_"] o.version;
-              name = "z3_${version}";
-              diff = ./patches + "/${version}.diff";
-              args =
-                o
-                // (
-                  if builtins.pathExists diff
-                  then {patches = [diff];}
-                  else {}
-                );
-              value = pkgs.callPackage (z3-factory args) {
-                python = pkgs.python3;
-              };
-            in {inherit name value;}
-          )
-          (pkgs.lib.filter (o: !(o.broken or false)) versions));
+        # A set mapping a version of Z3 to its derivations
+        z3-set = lib.listToAttrs (map (o:
+          let
+            version = lib.replaceStrings [ "." ] [ "_" ] o.version;
+            name = "z3_${version}";
+            diff = ./patches + "/${version}.diff";
+            args = o // (if builtins.pathExists diff then {
+              patches = [ diff ];
+            } else
+              { });
+            value =
+              pkgs.callPackage (z3-factory args) { python = pkgs.python3; };
+          in { inherit name value; })
+          (lib.filter (o: !(o.broken or false)) versions));
+        # Given a list of Z3 versions `[V1 ... VN]`, `mk-bundle [V1 ... VN]` is a derivation that contains binaries `z3-V1`, ..., `z3-VN`.
         mk-bundle = z3-versions:
           pkgs.stdenv.mkDerivation {
             name = "z3-bundle";
             unpackPhase = "true";
             buildPhase = "true";
-            installPhase = "
+            installPhase = ''
               mkdir -p $out/bin
-              ${pkgs.lib.strings.concatMapStringsSep "\n" (
-                version: let
-                  z3 = z3-set.${"z3_${pkgs.lib.replaceStrings ["."] ["_"] version}"};
-                  ver = pkgs.lib.replaceStrings ["_"] ["."] version;
-                in ''ln -s "${z3}/bin/z3" $out/bin/z3-${ver}''
-              )
-              z3-versions}
-            ";
+              ${lib.strings.concatMapStringsSep "\n" (version:
+                let
+                  z3 = z3-set.${
+                      "z3_${lib.replaceStrings [ "." ] [ "_" ] version}"
+                    };
+                  ver = lib.replaceStrings [ "_" ] [ "." ] version;
+                in ''ln -s "${z3}/bin/z3" $out/bin/z3-${ver}'') z3-versions}
+            '';
           };
       in {
-        packages =
-          z3-set
-          // {
-            z3-bundle = pkgs.stdenv.mkDerivation {
-              name = "z3-bundle";
-              unpackPhase = "true";
-              buildPhase = "true";
-              installPhase = "
-                  mkdir -p $out/bin
-                  echo '${builtins.toJSON (map (d: d.version) (pkgs.lib.attrValues z3-set))}' > $out/xx
-                ";
-            };
-          };
+        packages = z3-set // {
+          bundles = let
+            versions-list = map (o: o.version) versions;
+            mk = included:
+              let
+                bundle =
+                  if lib.length included == 0 then { } else mk-bundle included;
+                available =
+                  lib.filter (o: !(lib.elem o included)) versions-list;
+              in bundle // (if lib.length available == 0 then
+                { }
+              else
+                (lib.listToAttrs (map (version: {
+                  name = "z3_${lib.replaceStrings [ "." ] [ "_" ] version}";
+                  value = mk (included ++ [ version ]);
+                }) available)));
+          in mk [ ];
+        };
 
         lib.mk-z3-bundle = mk-bundle;
 
@@ -77,6 +71,5 @@
             ''}";
           };
         };
-      }
-    );
+      });
 }
